411bdea16dc9c61a7f1d0879d4541bf60d617459
Ilya Strukov <iley@iley.ru>
2020-12-05 18:21:59 +0100

    Day 3 Part 2

--- 03/part1.c
+++ 03/part1.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("the answer is %d\n", n_trees);
+    printf("the answer is %d", n_trees);
 
     return EXIT_SUCCESS;
 }
--- 03/part2.c
+++ 03/part2.c
@@ -1,7 +1,54 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+const int width = 31;
+const int height = 323;
+
+int count_trees(char *map, int dx, int dy) {
+    int n_trees = 0;
+    int x = 0;
+    for (int y = 0; y < height; y += dy) {
+        if (map[x + y*width] == '#') {
+            n_trees++;
+        }
+        x = (x + dx) % width;
+    }
+    return n_trees;
+}
+
+int main(int argc, char *argv[]) {
+    static char map[width*height];
+
+    int i = 0;
+
+    while (i < width*height) {
+        int ch = fgetc(stdin);
+        assert(ch != EOF);
+        if (ch == '.' || ch == '#') {
+            map[i] = ch;
+            i++;
+        }
+    }
+
+    const int n_ans = 5;
+    int ans[n_ans];
+
+    ans[0] = count_trees(map, 1, 1);
+    ans[1] = count_trees(map, 3, 1);
+    ans[2] = count_trees(map, 5, 1);
+    ans[3] = count_trees(map, 7, 1);
+    ans[4] = count_trees(map, 1, 2);
+
+    long long int mul = 1;
+
+    for (int i = 0; i < n_ans; i++) {
+        printf("answer #%d is %d\n", i+1, ans[i]);
+        mul *= ans[i];
+    }
+
+    printf("the multiple is %lld\n", mul);
+
     return EXIT_SUCCESS;
 }
