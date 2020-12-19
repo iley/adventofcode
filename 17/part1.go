2965ff0664ea9335c8d7a307ac1090252251c347
Ilya Strukov <iley@iley.ru>
2020-12-19 16:06:38 +0100

    Day 17 Part 1

--- 17/part1.go
+++ 17/part1.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+const steps = 6
+
 type grid struct {
 	m          map[string]bool
 	minx, maxx int
@@ -53,6 +55,10 @@ func (g *grid) get(x, y, z int) bool {
 	return g.m[key]
 }
 
+func (g *grid) activeCount() int {
+	return len(g.m)
+}
+
 func load(input io.Reader) *grid {
 	g := newGrid()
 	scanner := bufio.NewScanner(input)
@@ -64,8 +70,44 @@ func load(input io.Reader) *grid {
 				g.set(x, y, 0, true)
 			}
 		}
+		y++
 	}
 	return g
+}
+
+func neighbours(g *grid, x, y, z int) int {
+	n := 0
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			for dz := -1; dz <= 1; dz++ {
+				if dx == 0 && dy == 0 && dz == 0 {
+					continue
+				}
+
+				if g.get(x+dx, y+dy, z+dz) {
+					n++
+				}
+			}
+		}
+	}
+	return n
+}
+
+func step(g *grid) *grid {
+	ng := newGrid()
+	for x := g.minx - 1; x <= g.maxx+1; x++ {
+		for y := g.miny - 1; y <= g.maxy+1; y++ {
+			for z := g.minz - 1; z <= g.maxz+1; z++ {
+				n := neighbours(g, x, y, z)
+				if n == 3 {
+					ng.set(x, y, z, true)
+				} else if n == 2 {
+					ng.set(x, y, z, g.get(x, y, z))
+				}
+			}
+		}
+	}
+	return ng
 }
 
 func main() {
@@ -86,4 +128,10 @@ func main() {
 
 	fmt.Printf("loaded grid %dx%dx%d\n", (g.maxx - g.minx + 1), (g.maxy - g.miny + 1), (g.maxz - g.minz + 1))
 
+	for i := 0; i < steps; i++ {
+		g = step(g)
+	}
+
+	answer := g.activeCount()
+	fmt.Println(answer)
 }
