795a1e4275c3be63e5b08a191374322d42814ff5
Ilya Strukov <iley@iley.ru>
2023-12-09 07:02:56 +0000

    9/2

--- 2023/bin/d09/d09.ml
+++ 2023/bin/d09/d09.ml
@@ -17,18 +17,28 @@ let remove_last_element numbers =
 let diff numbers =
   List.map2_exn (remove_last_element numbers) (List.tl_exn numbers) ~f:(fun a b -> b - a)
 
-let rec approximate numbers =
+let rec extrapolate numbers =
   if all_zeroes numbers then
     0
   else
-    (List.last_exn numbers) + (approximate (diff numbers))
+    (List.last_exn numbers) + (extrapolate (diff numbers))
+
+let rec extrapolate_back numbers =
+  if all_zeroes numbers then
+    0
+  else
+    (List.hd_exn numbers) - (extrapolate_back (diff numbers))
 
 let part1 input =
-  List.map input ~f:approximate |> List.fold ~init:0 ~f:(+)
+  List.map input ~f:extrapolate |> List.fold ~init:0 ~f:(+)
+
+let part2 input =
+  List.map input ~f:extrapolate_back |> List.fold ~init:0 ~f:(+)
 
 let () =
   let input = read_input "bin/d09/input.txt" in
   let parsed_input = parse_input input in
   begin
-    printf "Part 1: %d" (part1 parsed_input)
+    printf "Part 1: %d\n" (part1 parsed_input);
+    printf "Part 2: %d\n" (part2 parsed_input)
   end
