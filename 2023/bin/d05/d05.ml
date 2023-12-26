4dd45e5805fc249a0c8571251708da00ee792ca3
Ilya Strukov <iley@iley.ru>
2023-12-26 21:43:24 +0000

    2023

--- 2023/bin/d05/d05.ml
+++ 2023/bin/d05/d05.ml
@@ -65,24 +65,95 @@ let translate (mapping : mapping_t) value =
     !result
   end
 
+let translate_back (mapping : mapping_t) value =
+  let result = ref value in
+  let found = ref false in
+  begin
+    List.iter mapping ~f:(fun range ->
+      if not !found && (value >= range.dst_start && value < range.dst_start + range.len) then (
+        let translated = range.src_start + (value - range.dst_start) in
+        if (translate mapping translated) = value then (
+          result := translated;
+          found := true
+        )
+      )
+    );
+    !result
+  end
+
 let part1 almanach =
   let locations = List.map almanach.seeds ~f:(fun seed ->
-    printf "%d -> " seed;
     let result = ref seed in
     begin
       List.iter almanach.mappings ~f:(fun mapping ->
         result := translate mapping !result;
-        printf "%d -> " !result;
       );
-      printf "\n";
       !result;
     end
   ) in
   List.min_elt locations ~compare:Int.compare |> Option.value_exn
 
+let _ = part1
+
+type seed_range_t = {
+  start: int;
+  len: int;
+}
+
+let seed_range_to_str range =
+  sprintf "%d-%d" range.start (range.start + range.len)
+
+let make_seed_ranges seeds =
+  let rec aux seeds ranges =
+    match seeds with
+    | [] -> ranges
+    | x :: y :: xs -> aux xs ({ start = x; len = y } :: ranges)
+    | _ :: [] -> failwith "Unexpected end of list"
+  in
+  aux seeds []
+
+let is_valid_seed seed ranges =
+  List.exists ranges ~f:(fun range ->
+    seed >= range.start && seed <= range.start + range.len
+  )
+
+let seed_from_location almanach location =
+  let result = ref location in
+  begin
+    printf "%d -> " !result;
+    List.iter almanach.mappings ~f:(fun mapping ->
+      result := translate_back mapping !result;
+      printf "%d -> " !result;
+    );
+    printf "\n";
+    !result
+  end
+
+let part2 almanach =
+  let loc = ref 1 in
+  let found = ref false in
+  let seed_ranges = make_seed_ranges almanach.seeds in
+  let () = printf "Seed ranges: %s\n" (List.map seed_ranges ~f:seed_range_to_str |> String.concat ~sep:", ") in
+  begin
+    while not !found do
+      if !loc % 1000 = 0 then (
+        printf "loc: %d\n" !loc;
+        Out_channel.flush stdout;
+      );
+      let seed = seed_from_location almanach !loc in
+      if is_valid_seed seed seed_ranges then (
+        printf "%d -> %d\n" !loc seed;
+        Out_channel.flush stdout;
+        found := true
+      ) else
+        loc := !loc + 1
+    done;
+    !loc
+  end
+
 let () =
   let input = read_input "bin/d05/input.txt" in
   let almanach = parse_input input in
   begin
-    printf "Part 1: %d\n" (part1 almanach);
+    printf "Part 2: %d\n" (part2 almanach);
   end
--- 2023/py/d10/solution.py
+++ 2023/py/d10/solution.py
@@ -53,6 +53,94 @@ def loop_length(node: tuple[int, int]):
         else:
             return steps
 
-length = loop_length(start)
-print("Loop length is %d" % length)
-print("Part 1 solution is %d"  % ((length + 1) // 2))
+grid = [[ ch for ch in line ] for line in lines]
+grid2 = [[ ch for ch in line ] for line in lines]
+
+def rotate_right(direction: tuple[int, int]):
+    if direction == (1, 0):
+        return (0, -1)
+    if direction == (-1, 0):
+        return (0, 1)
+    if direction == (0, 1):
+        return (1, 0)
+    if direction == (0, -1):
+        return (-1, 0)
+    raise Exception("Invalid direction")
+
+def rotate_left(direction: tuple[int, int]):
+    if direction == (1, 0):
+        return (0, 1)
+    if direction == (-1, 0):
+        return (0, -1)
+    if direction == (0, 1):
+        return (-1, 0)
+    if direction == (0, -1):
+        return (1, 0)
+    raise Exception("Invalid direction")
+
+def traverse(node: tuple[int, int]):
+    visited = set([node])
+    steps = 0
+    while True:
+        row, col = node
+        grid[row][col] = "#"
+        possible_next_nodes = [n for n in m[node] if n not in visited]
+        if possible_next_nodes:
+            node = possible_next_nodes[0]
+            drow, dcol = node[0] - row, node[1] - col
+            dir = (drow, dcol)
+            dir_side = rotate_right(dir)
+            side_node = (row + dir_side[0], col + dir_side[1])
+            grid2[side_node[0]][side_node[1]] = "I"
+
+            visited.add(node)
+            steps += 1
+        else:
+            return steps
+
+# length = loop_length(start)
+# print("Loop length is %d" % length)
+# print("Part 1 solution is %d"  % ((length + 1) // 2))
+
+traverse(start)
+
+for row in range(len(grid2)):
+    for col in range(len(grid2[row])):
+        if grid[row][col] != "#" and grid2[row][col] == "I":
+            grid[row][col] = "I"
+
+while True:
+    changed = False
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == "I":
+                if grid[row - 1][col] == ".":
+                    grid[row - 1][col] = "I"
+                    changed = True
+                if grid[row + 1][col] == ".":
+                    grid[row + 1][col] = "I"
+                    changed = True
+                if grid[row][col - 1] == ".":
+                    grid[row][col - 1] = "I"
+                    changed = True
+                if grid[row][col + 1] == ".":
+                    grid[row][col + 1] = "I"
+                    changed = True
+    if not changed:
+        break
+
+c = 0
+for row in range(len(grid)):
+    for col in range(len(grid[row])):
+        if grid[row][col] == "I":
+            c += 1
+print(c)
+
+for row in range(len(grid)):
+    for col in range(len(grid[row])):
+        if grid[row][col] != "I":
+            grid[row][col] = lines[row][col]
+
+with open("output.txt", "w") as f:
+    for line in grid:
+        f.write("".join(line))
