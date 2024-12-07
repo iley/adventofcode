29028c30b8bfe38a4443c3345df7de76d058e462
Ilya Strukov <ilya@istrukov.com>
2024-12-07 10:21:50 +0300

    Day 7 Part 1 attempt (not working yet)

---
+++ 2024/07/solution.c
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+// #include "sample.h"
+#include "input.h"
+
+#define ARRSIZE(x) (sizeof(x) / sizeof(x[0]))
+
+int operlen(long long *operands) {
+  int count = 0;
+  for (int i = 0; operands[i]; i++) {
+    count++;
+  }
+  return count;
+}
+
+bool can_be_true(long long value, long long *operands, int noperands) {
+  if (noperands == 1) {
+    return value == operands[0];
+  }
+
+  return can_be_true(value - operands[noperands - 1], operands,
+                     noperands - 1) ||
+         can_be_true(value / operands[noperands - 1], operands, noperands - 1);
+}
+
+long part1() {
+  long sum = 0;
+
+  for (int i = 0; i < ARRSIZE(values); i++) {
+    long long val = values[i];
+    long long *oper = operands[i];
+
+    if (can_be_true(val, oper, operlen(oper))) {
+      printf("YES %lld\n", val);
+      sum += val;
+    } else {
+      printf("NO  %lld\n", val);
+    }
+  }
+
+  return sum;
+}
+
+int main() {
+  printf("Part 1 solution: %ld\n", part1());
+  return 0;
+}
