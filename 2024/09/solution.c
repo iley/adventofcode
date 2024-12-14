6ab38aa5ef136c9c22cd0afe9a69ddb30d332a62
Ilya Strukov <ilya@istrukov.com>
2024-12-14 19:30:17 +0000

    9/2

--- 2024/09/solution.c
+++ 2024/09/solution.c
@@ -15,13 +15,13 @@ typedef struct {
   int len;
 } inode_t;
 
-int *makemap(const char *input) {
+int *make_map(const char *input) {
   int *map = NULL;
 
   int len = strlen(input);
   int id = 0;
-
   bool isfile = true;
+
   for (int i = 0; i < len; i++) {
     int blocks = input[i] - '0';
 
@@ -42,7 +42,36 @@ int *makemap(const char *input) {
   return map;
 }
 
-void printmap(int *map) {
+inode_t *make_inodes(const char *input) {
+  inode_t *inodes = NULL;
+
+  int len = strlen(input);
+  int id = 0;
+  bool isfile = true;
+  int pos = 0;
+
+  for (int i = 0; i < len; i++) {
+    int len = input[i] - '0';
+    inode_t inode = {
+        .pos = pos,
+        .len = len,
+    };
+    pos += len;
+    if (isfile) {
+      inode.id = id;
+      id++;
+    } else {
+      inode.id = -1;
+    }
+    arrpush(inodes, inode);
+
+    isfile = !isfile;
+  }
+
+  return inodes;
+}
+
+void print_map(int *map) {
   for (int i = 0; i < arrlen(map); i++) {
     if (map[i] == -1) {
       printf(".");
@@ -65,7 +94,7 @@ bool is_defrag_done(int *map) {
 }
 
 int *defrag(int *map) {
-  // printmap(map);
+  // print_map(map);
 
   while (!is_defrag_done(map)) {
     int rightmost_block = 0;
@@ -87,13 +116,62 @@ int *defrag(int *map) {
     int t = map[rightmost_block];
     map[rightmost_block] = map[leftmost_space];
     map[leftmost_space] = t;
-    // printmap(map);
+    // print_map(map);
   }
 
   return map;
 }
 
-int *defrag2(int *map) { return map; }
+int *inodes_to_map(inode_t *inodes) {
+  int len_sum = 0;
+  for (int i = 0; i < arrlen(inodes); i++) {
+    len_sum += inodes[i].len;
+  }
+
+  int *map = NULL;
+  arrsetlen(map, len_sum);
+  for (int i = 0; i < arrlen(map); i++) {
+    map[i] = -1;
+  }
+
+  for (int i = 0; i < arrlen(inodes); i++) {
+    for (int pos = inodes[i].pos; pos < inodes[i].pos + inodes[i].len; pos++) {
+      map[pos] = inodes[i].id;
+    }
+  }
+
+  return map;
+}
+
+int *defrag2(inode_t *inodes) {
+  for (int i = arrlen(inodes); i >= 0; i--) {
+    if (inodes[i].id == -1) {
+      continue;
+    }
+
+    // try finding an empty slot for this file.
+    for (int j = 0; j < arrlen(inodes); j++) {
+      if (inodes[j].id != -1 || inodes[j].pos >= inodes[i].pos ||
+          inodes[j].len < inodes[i].len) {
+        continue;
+      }
+
+      // found one
+      inodes[i].pos = inodes[j].pos;
+      inodes[j].pos += inodes[i].len;
+      inodes[j].len -= inodes[i].len;
+
+      if (i > 0) {
+        // update the space before the file
+        inodes[i - 1].len += inodes[i].len;
+      }
+    }
+  }
+
+  int *map = inodes_to_map(inodes);
+
+  return map;
+}
 
 long long checksum(int *map) {
   long long sum = 0;
@@ -109,7 +187,7 @@ long long checksum(int *map) {
 }
 
 long long part1(const char *input) {
-  int *map = makemap(input);
+  int *map = make_map(input);
 
   map = defrag(map);
 
@@ -119,11 +197,13 @@ long long part1(const char *input) {
 }
 
 long long part2(const char *input) {
-  int *map = makemap(input);
+  inode_t *inodes = make_inodes(input);
 
-  map = defrag2(map);
+  int *map = defrag2(inodes);
 
   long long answer = checksum(map);
+
+  arrfree(inodes);
   arrfree(map);
   return answer;
 }
