9acd05d5bafb12f53c7b955b0eeb6a28ef7b1469
Ilya Strukov <ilya@istrukov.com>
2026-01-01 11:06:10 +0000

    2025/02 part 2

--- 2025/02/solution.c
+++ 2025/02/solution.c
@@ -31,11 +31,52 @@ long part1(range_t input[], int n) {
   for (int i = 0; i < n; i++) {
     long start = input[i].start;
     long end = input[i].end;
-    printf("range %ld..%ld\n", start, end);
+    // printf("range %ld..%ld\n", start, end);
     for (long id = start; id <= end; id++) {
       if (is_invalid(id)) {
         sum += id;
-        printf("invalid id %ld, sum = %ld\n", id, sum);
+        // printf("invalid id %ld\n", id);
+      }
+    }
+  }
+  return sum;
+}
+
+bool is_repeated(long n, long div) {
+  long segment = n % div;
+  if (segment < div / 10) {
+    return false; // there are leading zeros, bail out
+  }
+  long rest = n / div;
+  while (rest > 0) {
+    long next_segment = rest % div;
+    rest = rest / div;
+    if (next_segment != segment) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool is_invalid_v2(long id) {
+  for (long div = 10; id / div != 0; div *= 10) {
+    if (is_repeated(id, div)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+long part2(range_t input[], int n) {
+  long sum = 0;
+  for (int i = 0; i < n; i++) {
+    long start = input[i].start;
+    long end = input[i].end;
+    // printf("range %ld..%ld\n", start, end);
+    for (long id = start; id <= end; id++) {
+      if (is_invalid_v2(id)) {
+        sum += id;
+        // printf("invalid id %ld\n", id);
       }
     }
   }
@@ -44,5 +85,6 @@ long part1(range_t input[], int n) {
 
 int main() {
   printf("part 1: %ld\n", part1(input, ARRLEN(input)));
+  printf("part 2: %ld\n", part2(input, ARRLEN(input)));
   return 0;
 }
